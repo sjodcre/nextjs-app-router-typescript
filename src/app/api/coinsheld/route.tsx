c2223350eececd8b6741ec46d61124634908313c
AnthonyBatato <brianboudeville@gmail.com>
2024-04-29 14:34:43 +0800

    updated profile and added update username

---
+++ src/app/api/coinsheld/route.tsx
@@ -0,0 +1,34 @@
+import { query } from "../db";
+
+
+
+
+export async function GET(req: Request, route: { params: { id: string } }) {
+  try {
+
+    const url = new URL(req.url)
+
+    const chain= url.searchParams.get("chain")
+    const id= url.searchParams.get("id")
+   // const id = route.params.id;
+    // Fetch token data based on the ID from the database using parameterized query
+    const profile = await query(`
+    SELECT *
+    FROM coinsheld_${chain} 
+      where walletaddress = '${id}'
+    `, []);
+
+    if (profile.length === 0) {
+      
+      // If no token is found with the specified ID, return a 404 status code
+      return new Response(JSON.stringify(`Coin not found: ${id}`), { status: 404 });
+    }
+
+    // If a token is found, return it as a JSON response with a 200 status code
+    return new Response(JSON.stringify(profile), { status: 200 });
+  } catch (error) {
+    console.error('Error fetching Coin:', error);
+    // If an error occurs during fetching, return a 500 status code
+    return new Response(JSON.stringify('Internal Server Error'), { status: 500 });
+  }
+}
