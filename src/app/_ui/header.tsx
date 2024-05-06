8d09ba4b82d360c4e01d6de78f5ae3719fb264b9
AnthonyBatato <brianboudeville@gmail.com>
2024-05-06 10:18:41 +0800

    change main page ui

--- src/app/_ui/header.tsx
+++ src/app/_ui/header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
     const profile = "0x742d35cc6634c0532925a3b844bc454e4438f44e"; 
 
   return (
-    <header className="bg-blue-500 text-white py-4 flex justify-between items-center">
+    <header className=" text-white py-4 flex justify-between items-center">
       <div className="container mx-auto">
-        <h1 className="text-xl font-bold">Your Website</h1>
+       
       </div>
       <div className="mr-4">
         <Link href={`/profile/${profile}`}>
-          View Profile
+         | View Profile |
         </Link>
       </div>
     </header>
