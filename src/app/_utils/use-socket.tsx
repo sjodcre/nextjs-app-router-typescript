3fff734a82c8925a6daee706c6baee481328bef7
Your Name <you@example.com>
2024-07-25 14:17:11 +0800

    remove local socket setup

--- src/app/_utils/use-socket.tsx
+++ src/app/_utils/use-socket.tsx
@@ -70,10 +70,10 @@ const useSocket = (listeners: EventListener[] = []) => {
       // const tradeTime = parseInt(tradeTimeStr);
       // const tradePrice = parseFloat(data.deposit) / parseFloat(data.amount);
       const tradePrice = data.bondingPrice;
-      console.log("timestamp given", data.timestamp)
+      // console.log("timestamp given", data.timestamp)
       // const tradePrice = calculatePrice()
       const tradeTime = parseInt(data.timestamp);
-      console.log("tradeTime", tradeTime)
+      // console.log("tradeTime", tradeTime)
 
       // const channelString = `0~${exchange}~${fromSymbol}~${toSymbol}`;
       let descriptionSnippet = data.token_description.substring(0, 10);
@@ -110,8 +110,6 @@ const useSocket = (listeners: EventListener[] = []) => {
       const tradeBarTime = getFiveMinuteBarStartTime(tradeTime);
 
       let bar: any = {};
-      console.log("tradeBarTime", tradeBarTime)
-      console.log("lastBar time", lastBar.time)
       if (tradeBarTime !== (lastBar.time/1000)) {
         bar = {
           time: tradeBarTime * 1000,
@@ -181,7 +179,7 @@ export function subscribeOnStream(
 )
 {
 
-  const parsedSymbol = parseFullSymbol(`${symbolInfo.exchange}:${symbolInfo.name}`);
+  // const parsedSymbol = parseFullSymbol(`${symbolInfo.exchange}:${symbolInfo.name}`);
   let descriptionSnippet = symbolInfo?.description.substring(0, 10);
 
   const channelString = `0~${symbolInfo?.ticker}~${symbolInfo?.name}~${descriptionSnippet}`;
